0f91758642fb1744c537fd7c863eb003cbbf2435
Abhinav Nimma <animma@dxo.com>
2023-05-04 23:32:44 +0200

    Add files

---
+++ pages/4 🤖Data Modelling.py
@@ -0,0 +1,3 @@
+import streamlit as stl 
+stl.set_page_config(layout="wide")
+stl.text("UNDER CONSTRUCTION 🔧⚙️")
---
+++ pages/6 ☁️Model Deployment.py
@@ -0,0 +1,4 @@
+import streamlit as stl 
+stl.set_page_config(layout="wide")
+
+stl.text("UNDER CONSTRUCTION 🔧⚙️")
